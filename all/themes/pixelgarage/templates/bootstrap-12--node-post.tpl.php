3abcc69a1800beee3a8c1287ce4c833f8bfe71b3
Ralph Moser <ralph@ramosoft.ch>
2016-09-30 22:09:21 +0200

    - first version Core->7.50

---
+++ all/themes/pixelgarage/templates/bootstrap-12--node-post.tpl.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * @file
+ * Bootstrap 12 template for Display Suite.
+ */
+//
+// set language dependent heading
+$content = $variables['content'];
+$post_title = t('Support campaign!');
+$post_heading = t('Share this post with your friends and family members!');
+$post_claim = t('www.akw.sucks');
+?>
+
+
+<?php if (!$teaser): ?>
+<div class="node-post-page-wrapper">
+    <div class="node-post-header">
+      <div class="post-title">
+        <?php print $post_title ?>
+      </div>
+      <div class="post-heading">
+        <?php print $post_heading ?>
+      </div>
+      <!-- AddtoAny social share buttons
+      <div class="social-share">
+        <div class="a2a_kit a2a_kit_size_32 addtoany_list">
+          <a class="a2a_button_facebook"></a>
+          <a class="a2a_button_twitter"></a>
+          <a class="a2a_button_whatsapp"></a>
+        </div>
+        <script async src="https://static.addtoany.com/menu/page.js"></script>
+      </div>
+      -->
+    </div>
+    <div class="node-post-wrapper">
+<?php endif; ?>
+
+<<?php print $layout_wrapper; print $layout_attributes; ?> class="<?php print $classes; ?>">
+  <?php if (isset($title_suffix['contextual_links'])): ?>
+    <?php print render($title_suffix['contextual_links']); ?>
+  <?php endif; ?>
+  <div class="row">
+    <<?php print $central_wrapper; ?> class="col-sm-12 <?php print $central_classes; ?>">
+      <?php print render($content['field_image']); ?>
+      <?php if (!empty($post_claim)): ?>
+        <div class="quote-wrapper">
+          <?php print render($content['field_quote']); ?>
+          <span class="post-claim"><?php print $post_claim; ?></span>
+        </div>
+      <?php else: ?>
+        <?php print render($content['field_quote']); ?>
+      <?php endif; ?>
+      <?php print render($content['field_your_name']); ?>
+      <?php print render($content['group_icons']); ?>
+    </<?php print $central_wrapper; ?>>
+  </div>
+</<?php print $layout_wrapper ?>>
+
+<?php if (!$teaser): ?>
+  <!-- close node wrapper and node-page-wrapper-->
+  </div>
+</div>
+<?php endif; ?>
+
+
+
+<!-- Needed to activate display suite support on forms -->
+<?php if (!empty($drupal_render_children)): ?>
+  <?php print $drupal_render_children ?>
+<?php endif; ?>
